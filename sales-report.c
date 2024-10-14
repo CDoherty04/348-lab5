f68e10d707720e987d1395cee60d55b367c856f5
Charlie <charlieedoherty@gmail.com>
2024-10-13 19:51:33 -0500

    Add validation and formatting

--- sales-report.c
+++ sales-report.c
@@ -3,36 +3,56 @@
 #include <stdlib.h>
 
 // Get input for sales array
-void getInput(char *months[], float sales[]) {
-    for (int i = 0; i < 12; i++) {
-        printf("Enter sales for %s: ", months[i]);
-        scanf("%f", &sales[i]);
+void getInput(char *months[], float sales[])
+{
+    for (int i = 0; i < 12; i++)
+    {
+        while (1)
+        {
+            printf("Enter sales for %s: ", months[i]);
+            if (scanf("%f", &sales[i]) != 1)
+            {
+                printf("Invalid value. Please enter a valid number.\n");
+                // Clear the input buffer to avoid an infinite loop
+                while (getchar() != '\n');
+            }
+            else {
+                break;
+            }
+        }
     }
 }
 
 // Display the month and sales in two columns.
-void monthlySalesReport(char *months[], float sales[]) {
+void monthlySalesReport(char *months[], float sales[])
+{
     printf("Month\t\tSales\n");
 
     // Add extra tabs for shorter month names
-    for (int i = 0; i < 12; i++) {
-        if (strlen(months[i]) < 8) {
+    for (int i = 0; i < 12; i++)
+    {
+        if (strlen(months[i]) < 8)
+        {
             printf("%s\t\t$%.2f\n", months[i], sales[i]);
         }
-        else {
+        else
+        {
             printf("%s\t$%.2f\n", months[i], sales[i]);
         }
     }
 }
 
 // Helper function for salesSummaryReport
-void findMinSales(char* months[], float sales[]) {
+void findMinSales(char *months[], float sales[])
+{
     float min = sales[0];
     int minIndex = 0;
 
     // Compares every item to find minimum
-    for (int i = 1; i < 12; i++) {
-        if (sales[i] < min) {
+    for (int i = 1; i < 12; i++)
+    {
+        if (sales[i] < min)
+        {
             min = sales[i];
             minIndex = i;
         }
@@ -42,13 +62,16 @@ void findMinSales(char* months[], float sales[]) {
 }
 
 // Helper function for salesSummaryReport
-void findMaxSales(char* months[], float sales[]) {
+void findMaxSales(char *months[], float sales[])
+{
     float max = sales[0];
     int maxIndex = 0;
 
     // Compares every item to find maximum
-    for (int i = 1; i < 12; i++) {
-        if (sales[i] > max) {
+    for (int i = 1; i < 12; i++)
+    {
+        if (sales[i] > max)
+        {
             max = sales[i];
             maxIndex = i;
         }
@@ -58,19 +81,22 @@ void findMaxSales(char* months[], float sales[]) {
 }
 
 // Helper function for salesSummaryReport
-void findAverageSales(float sales[]) {
+void findAverageSales(float sales[])
+{
     float sum = sales[0];
-    
+
     // Sum every element and divide by the number of months
-    for (int i = 1; i < 12; i++) {
-            sum += sales[i];
+    for (int i = 1; i < 12; i++)
+    {
+        sum += sales[i];
     }
 
-    printf("Average sales:\t$%.2f\n", sum/12);
+    printf("Average sales:\t$%.2f\n", sum / 12);
 }
 
 // Show the minimum, maximum, and average monthly sales
-void salesSummaryReport(char *months[], float sales[]) {
+void salesSummaryReport(char *months[], float sales[])
+{
     printf("\nSales Summary Report:\n\n");
     findMinSales(months, sales);
     findMaxSales(months, sales);
@@ -78,22 +104,27 @@ void salesSummaryReport(char *months[], float sales[]) {
 }
 
 // Show average sales over all 7 six-month periods of the year
-void sixMonthMovingAverageReport(char *months[], float sales[]) {
+void sixMonthMovingAverageReport(char *months[], float sales[])
+{
     printf("\nSix-Month Moving Average Report:\n\n");
     float sum = 0;
 
     // 7 month "ranges"
-    for (int i = 0; i <= 6; i++) {
+    for (int i = 0; i <= 6; i++)
+    {
 
         // find averages for 6 month periods and output
-        for (int j = i; j < i+6; j++) {
+        for (int j = i; j < i + 6; j++)
+        {
             sum += sales[j];
         }
-        if (strlen(months[i]) < 16) {
-            printf("%s-%s\t\t$%.2f\n", months[i], months[i+5], sum/6);
+        if (strlen(months[i]) < 16)
+        {
+            printf("%s-%s\t\t$%.2f\n", months[i], months[i + 5], sum / 6);
         }
-        else {
-            printf("%s-%s\t$%.2f\n", months[i], months[i+5], sum/6);
+        else
+        {
+            printf("%s-%s\t$%.2f\n", months[i], months[i + 5], sum / 6);
         }
 
         // Reset sum before next iteration
@@ -102,12 +133,16 @@ void sixMonthMovingAverageReport(char *months[], float sales[]) {
 }
 
 // Display the sales from highest to lowest
-void highToLowSalesReport(char *months[], float sales[]) {
-    
-    // Descending bubble sort algorithm 
-    for (int i = 0; i < 11; i++) {
-        for (int j = 0; j < 11 - i; j++) {
-            if (sales[j] < sales[j + 1]) {
+void highToLowSalesReport(char *months[], float sales[])
+{
+
+    // Descending bubble sort algorithm
+    for (int i = 0; i < 11; i++)
+    {
+        for (int j = 0; j < 11 - i; j++)
+        {
+            if (sales[j] < sales[j + 1])
+            {
 
                 // Switch sales values
                 float tempSale = sales[j];
@@ -115,7 +150,7 @@ void highToLowSalesReport(char *months[], float sales[]) {
                 sales[j + 1] = tempSale;
 
                 // Switch months values
-                char* tempMonth = months[j];
+                char *tempMonth = months[j];
                 months[j] = months[j + 1];
                 months[j + 1] = tempMonth;
             }
@@ -126,9 +161,10 @@ void highToLowSalesReport(char *months[], float sales[]) {
     monthlySalesReport(months, sales);
 }
 
-int main() {
+int main()
+{
     float sales[12];
-    char* months[12] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+    char *months[12] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
 
     // Get input
     getInput(months, sales);
