99ee1a0ee3ff268aad37acc270af621c5a99f692
Charlie <charlieedoherty@gmail.com>
2024-10-13 20:08:05 -0500

    Implement printMatrix

--- matrix-manipulation.c
+++ matrix-manipulation.c
@@ -3,23 +3,28 @@
 #define SIZE 5
 
 // This function should take two matrices as input and return their sum
-int addMatrices(int m1[][], int m2[][]) {
-
+int addMatrices(int m1[SIZE][SIZE], int m2[SIZE][SIZE]) {
+    return 0;
 }
 
 // This function should take two matrices as input and return their product
-int multiplyMatrices(int m1[][], int m2[][]) {
-
+int multiplyMatrices(int m1[SIZE][SIZE], int m2[SIZE][SIZE]) {
+    return 0;
 }
 
 // This function should take a matrix as input and return its transpose
-int transposeMatrix(int m1[][], int m2[][]) {
-
+int transposeMatrix(int m1[SIZE][SIZE], int m2[SIZE][SIZE]) {
+    return 0;
 }
 
 // This function should take a matrix as input and print it in a user-friendly format
-int printMatrix(int m1[][], int m2[][]) {
-
+void printMatrix(int m[SIZE][SIZE]) {
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            printf("%d\t", m[i][j]);
+        }
+        printf("\n");
+    }
 }
 
 int main()
@@ -36,4 +41,12 @@ int main()
         {15, 14, 13, 12, 11},
         {10, 9, 8, 7, 6},
         {5, 4, 3, 2, 1}};
+
+    // addMatrices(m1, m2);
+    // multiplyMatrices(m1, m2);
+    // transposeMatrix(m1, m2);
+    printf("\nPrinting Matrix 1:\n");
+    printMatrix(m1);
+    printf("\nPrinting Matrix 2:\n");
+    printMatrix(m2);
 }
